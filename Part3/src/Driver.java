db8f566afd04a37d1fa1022cd596a6a1da79aa9b
Austin Carreño <80227315+AustinCarreno96@users.noreply.github.com>
2022-04-24 18:08:53 -0400

    Update Driver.java

--- Part3/src/Driver.java
+++ Part3/src/Driver.java
@@ -40,47 +40,5 @@ public class Driver {
         }// end while loop
     }// end main()
 }// end driver class
-=======
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.*;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
-public class Driver {
-    public static void main(String[] args) throws Exception {
-        InputStream inputStream = new FileInputStream(args[0]);
-        ANTLRInputStream input = new ANTLRInputStream(inputStream);
-
-        LittleLexer lexer = new LittleLexer(input);
-
-        // Removes standard error handling (would recover errors)
-        lexer.removeErrorListeners();
-
-        // Custom error handler throws a RuntimeException on error
-        lexer.addErrorListener(new AnyErrorListener());
-
-        CommonTokenStream token = new CommonTokenStream(lexer);
-        LittleParser parser = new LittleParser(token);
-
-        parser.removeErrorListeners();
-
-        SymbolExtractor SymbolExtractor = new SymbolExtractor();
-        ParseTreeWalker walker = new ParseTreeWalker();
-        ParseTree tree = parser.program();
-
-        walker.walk(SymbolExtractor,tree);
-        LinkedHashMap<String, SymbolTable> map = SymbolExtractor.getSymbolTable();
-        Iterator<Map.Entry<String, SymbolTable>> iterator = map.entrySet().iterator();
-
-        while(iterator.hasNext()){
-            Map.Entry<String, SymbolTable> pair = iterator.next();
-            SymbolTable symbolTable = pair.getValue();
-            symbolTable.printTable();
-            iterator.remove();
-        }// end while loop
-    }// end main()
-}// end driver class
 
